428482289cadcf76a983c7edf1b4fdecd24da13e
aishalieva0 <aysalieva6@gmal.com>
2023-12-29 18:25:29 -0800

    some files

--- tokenizers.c
+++ tokenizers.c
@@ -18,29 +18,22 @@ char **hsh_tokenizer(char *input)
 	char **tokens = NULL, *token = NULL;
 	int buffer = BUFSIZE, newBuffer = 0, position = 0;
 	char **backup_tokens = NULL;
-	/*allocate memory for the array of tokens */
 	tokens = malloc(buffer * sizeof(char *));
 	if (tokens == NULL)
 	{
 		fprintf(stderr, "memory allocation error\n");
 		exit(EXIT_FAILURE);
 	}
-	/* tokenize the input string, checking for delimiter to split*/
-	/*the string and replace them with \0 */
 	token = strtok(input, DELIM);
 	while (token != NULL)
 	{
-		/*the array of tokens and the position variable is incremeneted*/
 		tokens[position] = token;
 		position++;
-		/*if position variable is greater than or qual to buffer size, reallocate*/
-		/*memory for the array of tokens*/
 		if (position >= buffer)
 		{
 			newBuffer = BUFSIZE * 2;
 			backup_tokens = tokens;
 			tokens = _realloc(tokens, buffer, newBuffer * sizeof(char *));
-			/*if fail,frees backup array of tokens,frees current token,& prints error*/
 			if (tokens == NULL)
 			{
 				free(backup_tokens);
@@ -49,10 +42,8 @@ char **hsh_tokenizer(char *input)
 				exit(EXIT_FAILURE);
 			}
 		}
-		/*once memory reallocation, continue processing the input string */
 		token = strtok(NULL, DELIM);
 	}
-	/*null terminate the array of tokens */
 	tokens[position] = NULL;
 	return (tokens);
 }
@@ -67,24 +58,19 @@ char **tokenizer_path(char *input)
 	int buffer = BUFSIZE, newBuffer = 0, position = 0;
 	char **tokens = NULL, *token = NULL;
 	char **backup_tokens = NULL;
-	/*allocate memory for the array of tokens */
 	tokens = malloc(buffer * sizeof(char *));
 	if (tokens == NULL)
 	{
 		fprintf(stderr, "memory allocation error\n");
 		exit(EXIT_FAILURE);
 	}
-	/*tokenize the PATH string, checking for deliminter :and replace it*/
-	/*with \0 and stores resulting tokens i nthe tokens array*/
 	token = strtok(input, ":");
 	while (token != NULL)
 	{
 		tokens[position] = token;
 		position++;
-		/*reallocate the array of tokens if necessary*/
 		if (position >= buffer)
 		{
-			/*doubles the buffer size*/
 			newBuffer = BUFSIZE * 2;
 			backup_tokens = tokens;
 			tokens = _realloc(tokens, buffer, newBuffer * sizeof(char *));
@@ -92,16 +78,12 @@ char **tokenizer_path(char *input)
 			{
 				free(backup_tokens);
 				free(tokens);
-				/*if error during realloc print error */
 				fprintf(stderr, "memory allocation error\n");
 				exit(EXIT_FAILURE);
 			}
 		}
-		/*once memory reallocation, continue processing the PATH string */
 		token = strtok(NULL, ":");
 	}
-	/*null terminate the array of tokens and*/
-	/*returns a pointer to the tokens array */
 	tokens[position] = NULL;
 	return (tokens);
 }
